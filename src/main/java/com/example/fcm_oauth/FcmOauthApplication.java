26f0c5d7875a1d584a8afabc130e07b3dd58ada0
choidongkuen <danaver12@daum.net>
2023-07-06 13:15:19 +0900

    initial commit

---
+++ src/main/java/com/example/fcm_oauth/FcmOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.fcm_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcmOauthApplication.class, args);
+    }
+
+}
